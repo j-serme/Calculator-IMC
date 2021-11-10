3f173516d9350475c7199c30765c898d6387a022
Julien Serme <julienserme@yahoo.com>
2021-11-10 16:05:13 +0100

    Reset des inputs

--- main2.js
+++ main2.js
@@ -1,18 +1,21 @@
 "use strict";
 
 function calculer() {
-    let taille = parseInt(document.querySelector("#taille").value);
-    let poids = parseInt(document.querySelector("#poids").value);
-    let imc = poids / (taille / 100) ** 2;
-    if (isNaN(taille) || isNaN(poids)) {
-        alert(`Veuillez entrer des valeurs de taille et de poids`);
-    } else {
-        document.querySelector(
-            "#result"
-        ).innerHTML = `Votre IMC est de ${imc.toFixed(2)}`;
-    }
+  let taille = parseInt(document.querySelector("#taille").value);
+  let poids = parseInt(document.querySelector("#poids").value);
+  let imc = poids / (taille / 100) ** 2;
+
+  if (isNaN(taille) || isNaN(poids)) {
+    alert(`Veuillez entrer des valeurs de taille et de poids`);
+  } else {
+    document.querySelector(
+      "#result"
+    ).innerHTML = `Votre IMC est de ${imc.toFixed(2)}`;
+  }
+  document.getElementById("taille").value = "";
+  document.getElementById("poids").value = "";
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("a").addEventListener("click", calculer);
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#calculer").addEventListener("click", calculer);
 });
